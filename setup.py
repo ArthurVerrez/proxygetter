75b14018e6cbeeaa7dae4cf1169b398ac73a3299
Arthur <arthurverrez@gmail.com>
2023-09-05 20:03:20 +0200

    chore: update pypi classifiers

--- setup.py
+++ setup.py
@@ -9,4 +9,10 @@ setup(
     description="A utility to get and filter free proxies",
     packages=find_packages(),
     install_requires=["requests", "beautifulsoup4"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.10",
+    ],
 )
