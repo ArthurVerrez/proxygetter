28dd4a6193b7feb9756472c6ff070edb580de21b
Arthur <arthurverrez@gmail.com>
2023-09-05 21:21:52 +0200

    doc: update gitignore and add description of project in pypi

--- setup.py
+++ setup.py
@@ -6,6 +6,10 @@ def read_requirements():
         return [line.strip() for line in req_file.readlines()]
 
 
+# Read the contents of your README file
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name="proxygetter",
     version="0.1.0",
@@ -13,6 +17,8 @@ setup(
     author="Arthur Verrez",
     author_email="macdouglass@outlook.com",
     description="A utility to get and filter free proxies",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     packages=find_packages(),
     install_requires=read_requirements(),
     classifiers=[
