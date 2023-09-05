b068c404f9d49b3aedffbdff86cb0b428939c4cd
Arthur <arthurverrez@gmail.com>
2023-09-05 21:24:07 +0200

    fix: update pypi package version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="proxygetter",
-    version="0.1.0",
+    version="0.1.1",
     url="https://github.com/ArthurVerrez/proxygetter",
     author="Arthur Verrez",
     author_email="macdouglass@outlook.com",
